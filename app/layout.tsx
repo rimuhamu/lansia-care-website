8bd17e43b212b18c1487b9a705a41d1ef27f7416
Emir Risyad <emirrsyd@gmail.com>
2025-09-19 16:50:21 +0200

    feat: change logo and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,32 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import type { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import { Analytics } from '@vercel/analytics/next';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Lansia Care Sleman',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+  description:
+    'Lansia Care Sleman menyediakan layanan perawatan dan pendampingan lansia dengan kasih sayang, aman, serta profesional di wilayah Sleman.',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html
-      lang="en"
+      lang='en'
       className={`${GeistSans.variable} ${GeistMono.variable}`}
-      suppressHydrationWarning
-    >
-      <body className="font-sans">
+      suppressHydrationWarning>
+      <body className='font-sans'>
         {children}
         <Analytics />
       </body>
     </html>
-
-
-  )
+  );
 }
